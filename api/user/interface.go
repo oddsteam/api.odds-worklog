e3734fbc2681b51a84321fa710f48edb4eaafd2d
Suphakrit Nakhabat <suphakrit@odds.team>
2018-10-09 22:57:21 +0700

    Add user interface

---
+++ api/user/interface.go
@@ -0,0 +1,17 @@
+package user
+
+import "gitlab.odds.team/worklog/api.odds-worklog/models"
+
+type Repository interface {
+	createUser(u *models.User) (*models.User, error)
+	// getUserByID(id int64) (*models.User, error)
+	// update(u *models.User) (*models.User, error)
+	// delete(id int64) (bool, error)
+}
+
+type Usecase interface {
+	createUser(u *models.User) (*models.User, error)
+	// getUserByID(id int64) (*models.User, error)
+	// update(u *models.User) (*models.User, error)
+	// delete(id int64) (bool, error)
+}
