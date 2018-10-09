1ea10937cd70e51c9886b997397fdeb47909ddb5
Suphakrit Nakhabat <suphakrit@odds.team>
2018-10-09 22:57:56 +0700

    Add user usecase

---
+++ api/user/usecase.go
@@ -0,0 +1,23 @@
+package user
+
+import (
+	"gitlab.odds.team/worklog/api.odds-worklog/models"
+)
+
+type usecase struct {
+	repo Repository
+}
+
+func newUsecase(r Repository) Usecase {
+	return &usecase{r}
+}
+
+func (u *usecase) createUser(m *models.User) (*models.User, error) {
+	user, err := u.repo.createUser(m)
+	if err != nil {
+		return nil, err
+	}
+
+	m.ID = user.ID
+	return m, nil
+}
