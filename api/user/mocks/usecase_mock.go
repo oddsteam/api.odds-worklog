effa82c6b953f9605b7be2cb3ab5d4716bd18bc8
Suphakrit Nakhabat <suphakrit@odds.team>
2018-10-15 10:48:25 +0700

    Update user usecase mock delete

--- api/user/mocks/usecase_mock.go
+++ api/user/mocks/usecase_mock.go
@@ -93,10 +93,10 @@ func (m *Usecase) DeleteUser(id string) error {
 	ret := m.Called(id)
 
 	var r1 error
-	if rf, ok := ret.Get(1).(func(string) error); ok {
+	if rf, ok := ret.Get(0).(func(string) error); ok {
 		r1 = rf(id)
 	} else {
-		r1 = ret.Error(1)
+		r1 = ret.Error(0)
 	}
 
 	return r1
