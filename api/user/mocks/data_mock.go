400413aef421ad0e0a3f7c077b29d30aae8200d6
yossatron <yossatron@yossatrons-MacBook-Pro.local>
2018-10-17 16:14:12 +0700

    create unittest getUser & getUserById  & login

--- api/user/mocks/data_mock.go
+++ api/user/mocks/data_mock.go
@@ -21,7 +21,41 @@ var (
 		Token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE0NjE5NTcxMzZ9.RB3arc4-OyzASAaUhC2W3ReWaXAt_z2Fd3BN4aWTgEY",
 	}
 
+	MockUserById = models.User{
+		ID:                "1234567890",
+		FullName:          "นายทดสอบชอบลงทุน",
+		Email:             "test@abc.com",
+		BankAccountName:   "ทดสอบชอบลงทุน",
+		BankAccountNumber: "123123123123",
+		TotalIncome:       "123123123",
+		SubmitDate:        "12/12/2561",
+		ThaiCitizenID:     "1234567890123",
+	}
+
 	userByte, _ = json.Marshal(MockUser)
 	UserJson    = string(userByte)
 	LoginJson   = `{"username": "root", "password":"1234"}`
+	Login       = models.Login{
+		Username: "root",
+		Password: "1234",
+	}
+
+	MockUsers = []*models.User{
+		{ID: "1234567890",
+			FullName:          "นายทดสอบชอบลงทุน",
+			Email:             "test@abc.com",
+			BankAccountName:   "ทดสอบชอบลงทุน",
+			BankAccountNumber: "123123123123",
+			TotalIncome:       "123123123",
+			SubmitDate:        "12/12/2561",
+			ThaiCitizenID:     "1234567890123"},
+		{ID: "1234567890",
+			FullName:          "นายไม่ชอบลงทุน",
+			Email:             "test@abc.com",
+			BankAccountName:   "ทดสอบชอบลงทุน",
+			BankAccountNumber: "123123123123",
+			TotalIncome:       "123123123",
+			SubmitDate:        "12/12/2561",
+			ThaiCitizenID:     "1234567890123"},
+	}
 )
--- api/user/usecase_test.go
+++ api/user/usecase_test.go
@@ -21,3 +21,38 @@ func TestCreateUser(t *testing.T) {
 	assert.Equal(t, mocks.MockUser.ID, u.ID)
 	mockRepo.AssertExpectations(t)
 }
+
+func TestGetUser(t *testing.T) {
+	mockRepo := new(mocks.Repository)
+	mockRepo.On("GetUser").Return(mocks.MockUsers, nil)
+
+	uc := newUsecase(mockRepo)
+	u, err := uc.GetUser()
+	assert.NoError(t, err)
+	assert.NotNil(t, u)
+	assert.Equal(t, mocks.MockUsers[0].FullName, u[0].FullName)
+	mockRepo.AssertExpectations(t)
+}
+
+func TestGetUserByID(t *testing.T) {
+	mockRepo := new(mocks.Repository)
+	mockRepo.On("GetUserByID", "1234567890").Return(&mocks.MockUserById, nil)
+
+	uc := newUsecase(mockRepo)
+	u, err := uc.GetUserByID(string(mocks.MockUserById.ID))
+	assert.NoError(t, err)
+	assert.NotNil(t, u)
+	assert.Equal(t, mocks.MockUserById.FullName, u.FullName)
+	mockRepo.AssertExpectations(t)
+}
+
+func TestLogin(t *testing.T) {
+	mockRepo := new(mocks.Repository)
+	mockRepo.On("Login", mock.AnythingOfType("*models.Login")).Return(&mocks.MockToken, nil)
+	uc := newUsecase(mockRepo)
+	u, err := uc.Login(&mocks.Login)
+	assert.NoError(t, err)
+	assert.NotNil(t, u)
+	assert.Equal(t, mocks.MockToken.Token, u.Token)
+	mockRepo.AssertExpectations(t)
+}
