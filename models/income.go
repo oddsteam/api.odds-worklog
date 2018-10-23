34254bbe0648db447d28a015def26c2c46d24592
Suphakrit Nakhabat <suphakrit@odds.team>
2018-10-23 16:21:48 +0700

    Add income model

--- models/income.go
+++ models/income.go
@@ -1,1 +1,13 @@
 package models
+
+import "gopkg.in/mgo.v2/bson"
+
+type Income struct {
+	ID          bson.ObjectId `bson:"_id" json:"id"`
+	UserID      string        `bson:"userId" json:"userId"`
+	TotalIncome string        `bson:"totalIncome" json:"totalIncome"`
+	SubmitDate  string        `bson:"submitDate" json:"submitDate"`
+	Note        string        `bson:"note" json:"note"`
+	VAT         string        `bson:"vat" json:"vat"`
+	WHT         string        `bson:"whr" json:"wht"`
+}
