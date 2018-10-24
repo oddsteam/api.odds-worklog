67ec8cdf0b9cb71d9e0155915d23f94b5367af65
Suphakrit Nakhabat <suphakrit@odds.team>
2018-10-24 10:21:07 +0700

    Update income repository mock

--- api/income/mocks/repository_mock.go
+++ api/income/mocks/repository_mock.go
@@ -9,22 +9,15 @@ type Repository struct {
 	mock.Mock
 }
 
-func (m *Repository) AddIncome(_u *models.Income) (*models.Income, error) {
+func (m *Repository) AddIncome(_u *models.Income) error {
 	ret := m.Called(_u)
 
-	var r0 *models.Income
-	if rf, ok := ret.Get(0).(func(*models.Income) *models.Income); ok {
-		r0 = rf(_u)
-	} else if ret.Get(0) != nil {
-		r0 = ret.Get(0).(*models.Income)
-	}
-
 	var r1 error
-	if rf, ok := ret.Get(1).(func(*models.Income) error); ok {
+	if rf, ok := ret.Get(0).(func(*models.Income) error); ok {
 		r1 = rf(_u)
 	} else {
-		r1 = ret.Error(1)
+		r1 = ret.Error(0)
 	}
 
-	return r0, r1
+	return r1
 }
