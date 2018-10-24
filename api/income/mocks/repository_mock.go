53f3a82ed80cb3a9228d3469bcdcc1a061b9b675
Suphakrit Nakhabat <suphakrit@odds.team>
2018-10-25 01:31:06 +0700

    Add income repo GetIncomeUserNow mock

--- api/income/mocks/repository_mock.go
+++ api/income/mocks/repository_mock.go
@@ -21,3 +21,23 @@ func (m *Repository) AddIncome(_u *models.Income) error {
 
 	return r1
 }
+
+func (m *Repository) GetIncomeUserNow(id, month string) (*models.Income, error) {
+	ret := m.Called(id, month)
+
+	var r0 *models.Income
+	if rf, ok := ret.Get(0).(func(string, string) *models.Income); ok {
+		r0 = rf(id, month)
+	} else if ret.Get(0) != nil {
+		r0 = ret.Get(0).(*models.Income)
+	}
+
+	var r1 error
+	if rf, ok := ret.Get(1).(func(string, string) error); ok {
+		r1 = rf(id, month)
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
