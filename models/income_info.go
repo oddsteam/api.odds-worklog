945c2b679a6f1ccf9bce64bd247156c48e773955
Suphakrit Nakhabat <suphakrit@odds.team>
2018-10-24 10:19:01 +0700

    Delete file unuse

--- models/income_info.go
+++
@@ -1,7 +0,0 @@
-package models
-
-type IncomeInfo struct {
-	User       User   `bson:"user" json:"user"`
-	SubmitDate string `bson:"submitDate" json:"submitDate"`
-	Status     string `bson:"status" json:"status"`
-}
