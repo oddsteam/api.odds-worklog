cef452743fd3f1dfe79f28718187e1f6b34a151f
Suphakrit Nakhabat <suphakrit@odds.team>
2018-10-24 23:41:50 +0700

    Update Income model

--- models/income.go
+++ models/income.go
@@ -6,6 +6,7 @@ type Income struct {
 	ID          bson.ObjectId `bson:"_id" json:"id"`
 	UserID      string        `bson:"userId" json:"userId"`
 	TotalIncome string        `bson:"totalIncome" json:"totalIncome"`
+	NetIncome   string        `bson:"netIncome" json:"netIncome"`
 	SubmitDate  string        `bson:"submitDate" json:"submitDate"`
 	Note        string        `bson:"note" json:"note"`
 	VAT         string        `bson:"vat" json:"vat"`
@@ -13,9 +14,9 @@ type Income struct {
 }
 
 type IncomeRes struct {
-	User       User   `bson:"user" json:"user"`
-	SubmitDate string `bson:"submitDate" json:"submitDate"`
-	Status     string `bson:"status" json:"status"`
+	User   *User   `bson:"user" json:"user,omitempty"`
+	Income *Income `bson:"income" json:"income,omitempty"`
+	Status string  `bson:"status" json:"status"`
 }
 
 type IncomeReq struct {
