ad95de2b1327a5d7bbf76b0d53541daf769121e5
Suphakrit Nakhabat <suphakrit@odds.team>
2018-10-24 10:24:17 +0700

    Implement AddIncome usecase

--- api/income/usecase.go
+++ api/income/usecase.go
@@ -1,21 +1,75 @@
 package income
 
 import (
+	"fmt"
+	"strconv"
+	"time"
+
+	"gitlab.odds.team/worklog/api.odds-worklog/api/user"
+
 	"gitlab.odds.team/worklog/api.odds-worklog/models"
 )
 
 type usecase struct {
-	repo Repository
+	repo     Repository
+	userRepo user.Repository
 }
 
-func newUsecase(r Repository) Usecase {
-	return &usecase{r}
+func newUsecase(r Repository, ur user.Repository) Usecase {
+	return &usecase{r, ur}
 }
 
-func (u *usecase) AddIncome(m *models.Income) (*models.Income, error) {
-	user, err := u.repo.AddIncome(m)
+func calVAT(income string) (string, error) {
+	num, err := strconv.ParseFloat(income, 64)
 	if err != nil {
-		return nil, err
+		return "", err
 	}
-	return user, nil
+	vat := float64(num * 0.07)
+	return fmt.Sprintf("%f", vat), nil
+}
+
+func calWHT(income string) (string, error) {
+	num, err := strconv.ParseFloat(income, 64)
+	if err != nil {
+		return "", err
+	}
+	vat := float64(num * 0.03)
+	return fmt.Sprintf("%f", vat), nil
+}
+
+func (u *usecase) AddIncome(req *models.IncomeReq, id string) error {
+	println(id)
+	us, err := u.userRepo.GetUserByID(id)
+	if err != nil {
+		println(err)
+		return err
+	}
+
+	var vat, wht = "", ""
+	vat, err = calVAT(req.TotalIncome)
+	if err != nil {
+		return err
+	}
+	if us.CorporateFlag == "Y" {
+		wht, err = calWHT(req.TotalIncome)
+		if err != nil {
+			return err
+		}
+	}
+
+	t := time.Now()
+
+	income := models.Income{
+		UserID:      id,
+		TotalIncome: req.TotalIncome,
+		SubmitDate:  t.Format(time.RFC3339),
+		Note:        req.Note,
+		VAT:         vat,
+		WHT:         wht,
+	}
+	err = u.repo.AddIncome(&income)
+	if err != nil {
+		return err
+	}
+	return nil
 }
--- api/income/usecase_test.go
+++ api/income/usecase_test.go
@@ -7,17 +7,47 @@ import (
 
 	"github.com/stretchr/testify/mock"
 	"gitlab.odds.team/worklog/api.odds-worklog/api/income/mocks"
+	userMocks "gitlab.odds.team/worklog/api.odds-worklog/api/user/mocks"
 )
 
-func TestAddIncome(t *testing.T) {
-	mockRepo := new(mocks.Repository)
-	mockRepo.On("AddIncome", mock.AnythingOfType("*models.Income")).Return(&mocks.MockIncome, nil)
+func TestCalVAT(t *testing.T) {
+	vat, err := calVAT("100000")
+	assert.NoError(t, err)
+	assert.Equal(t, "7000.000000", vat)
 
-	uc := newUsecase(mockRepo)
-	u, err := uc.AddIncome(&mocks.MockIncome)
+	vat, err = calVAT("123456")
+	assert.NoError(t, err)
+	assert.Equal(t, "8641.920000", vat)
+
+	vat, err = calVAT("99999")
+	assert.NoError(t, err)
+	assert.Equal(t, "6999.930000", vat)
+}
+
+func TestCalWHT(t *testing.T) {
+	vat, err := calWHT("100000")
+	assert.NoError(t, err)
+	assert.Equal(t, "3000.000000", vat)
+
+	vat, err = calWHT("123456")
+	assert.NoError(t, err)
+	assert.Equal(t, "3703.680000", vat)
+
+	vat, err = calWHT("99999")
+	assert.NoError(t, err)
+	assert.Equal(t, "2999.970000", vat)
+}
+
+func TestUsecaseAddIncome(t *testing.T) {
+	mockRepo := new(mocks.Repository)
+	mockRepo.On("AddIncome", mock.AnythingOfType("*models.Income")).Return(nil)
+
+	mockUserRepo := new(userMocks.Repository)
+	mockUserRepo.On("GetUserByID", mocks.MockIncome.UserID).Return(&userMocks.MockUserById, nil)
+
+	uc := newUsecase(mockRepo, mockUserRepo)
+	err := uc.AddIncome(&mocks.MockIncomeReq, mocks.MockIncome.UserID)
 
 	assert.NoError(t, err)
-	assert.NotNil(t, u)
-	assert.Equal(t, mocks.MockIncome.ID, u.ID)
 	mockRepo.AssertExpectations(t)
 }
