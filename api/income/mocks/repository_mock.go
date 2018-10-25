10e66b4aaa70d49cfb34875b514e569c7849bcad
Suphakrit Nakhabat <suphakrit@odds.team>
2018-10-26 01:01:40 +0700

    Update income repo mock

--- api/income/mocks/repository_mock.go
+++ api/income/mocks/repository_mock.go
@@ -41,3 +41,36 @@ func (m *Repository) GetIncomeUserNow(id, month string) (*models.Income, error) 
 
 	return r0, r1
 }
+
+func (m *Repository) GetIncomeByID(id1, id2 string) (*models.Income, error) {
+	ret := m.Called(id1, id2)
+
+	var r0 *models.Income
+	if rf, ok := ret.Get(0).(func(string, string) *models.Income); ok {
+		r0 = rf(id1, id2)
+	} else if ret.Get(0) != nil {
+		r0 = ret.Get(0).(*models.Income)
+	}
+
+	var r1 error
+	if rf, ok := ret.Get(1).(func(string, string) error); ok {
+		r1 = rf(id1, id2)
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
+func (m *Repository) UpdateIncome(income *models.Income) error {
+	ret := m.Called(income)
+
+	var r1 error
+	if rf, ok := ret.Get(0).(func(*models.Income) error); ok {
+		r1 = rf(income)
+	} else {
+		r1 = ret.Error(0)
+	}
+
+	return r1
+}
