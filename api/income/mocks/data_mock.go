64daaf1198e987c9f897c66313cb6a91ba17a916
Suphakrit Nakhabat <suphakrit@odds.team>
2018-10-26 01:00:25 +0700

    Update income data_mock

--- api/income/mocks/data_mock.go
+++ api/income/mocks/data_mock.go
@@ -3,12 +3,15 @@ package mocks
 import (
 	"encoding/json"
 
+	"gopkg.in/mgo.v2/bson"
+
 	userMocks "gitlab.odds.team/worklog/api.odds-worklog/api/user/mocks"
 	"gitlab.odds.team/worklog/api.odds-worklog/models"
 )
 
 var (
 	MockIncome = models.Income{
+		ID:          bson.ObjectIdHex("5bd1fda30fd2df2a3e41e569"),
 		UserID:      "5bbcf2f90fd2df527bc39539",
 		TotalIncome: "100000",
 		NetIncome:   "100000",
