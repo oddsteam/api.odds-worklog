75f9dbf42d1f211cd0912a1e94788c03db29b03d
Suphakrit Nakhabat <suphakrit@odds.team>
2018-10-26 01:01:51 +0700

    Update income usecase mock

--- api/income/mocks/usecase_mock.go
+++ api/income/mocks/usecase_mock.go
@@ -9,19 +9,39 @@ type Usecase struct {
 	mock.Mock
 }
 
-func (m *Usecase) AddIncome(income *models.IncomeReq, id string) (*models.IncomeRes, error) {
-	ret := m.Called(income, id)
+func (m *Usecase) AddIncome(income *models.IncomeReq, user *models.User) (*models.Income, error) {
+	ret := m.Called(income, user)
 
-	var r0 *models.IncomeRes
-	if rf, ok := ret.Get(0).(func(*models.IncomeReq, string) *models.IncomeRes); ok {
-		r0 = rf(income, id)
+	var r0 *models.Income
+	if rf, ok := ret.Get(0).(func(*models.IncomeReq, *models.User) *models.Income); ok {
+		r0 = rf(income, user)
 	} else if ret.Get(0) != nil {
-		r0 = ret.Get(0).(*models.IncomeRes)
+		r0 = ret.Get(0).(*models.Income)
 	}
 
 	var r1 error
-	if rf, ok := ret.Get(0).(func(*models.IncomeReq, string) error); ok {
-		r1 = rf(income, id)
+	if rf, ok := ret.Get(0).(func(*models.IncomeReq, *models.User) error); ok {
+		r1 = rf(income, user)
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
+func (m *Usecase) UpdateIncome(id string, req *models.IncomeReq, user *models.User) (*models.Income, error) {
+	ret := m.Called(id, req, user)
+
+	var r0 *models.Income
+	if rf, ok := ret.Get(0).(func(string, *models.IncomeReq, *models.User) *models.Income); ok {
+		r0 = rf(id, req, user)
+	} else if ret.Get(0) != nil {
+		r0 = ret.Get(0).(*models.Income)
+	}
+
+	var r1 error
+	if rf, ok := ret.Get(0).(func(string, *models.IncomeReq, *models.User) error); ok {
+		r1 = rf(id, req, user)
 	} else {
 		r1 = ret.Error(1)
 	}
