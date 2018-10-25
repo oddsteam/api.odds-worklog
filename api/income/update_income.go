f91b3aa6fedc7b948b7e7c6aa39eae6fda2f4a0e
Suphakrit Nakhabat <suphakrit@odds.team>
2018-10-26 01:05:11 +0700

    Add update_income usecase

---
+++ api/income/update_income.go
@@ -0,0 +1,25 @@
+package income
+
+import "gitlab.odds.team/worklog/api.odds-worklog/models"
+
+func (u *usecase) UpdateIncome(id string, req *models.IncomeReq, user *models.User) (*models.Income, error) {
+	income, err := u.repo.GetIncomeByID(id, user.ID.Hex())
+	if err != nil {
+		return nil, err
+	}
+
+	ins, err := calIncomeSum(req.TotalIncome, user.CorporateFlag)
+	if err != nil {
+		return nil, err
+	}
+
+	income.SubmitDate = getNow()
+	income.TotalIncome = req.TotalIncome
+	income.NetIncome = ins.Net
+	income.VAT = ins.VAT
+	income.WHT = ins.WHT
+	income.Note = req.Note
+	u.repo.UpdateIncome(income)
+
+	return income, nil
+}
