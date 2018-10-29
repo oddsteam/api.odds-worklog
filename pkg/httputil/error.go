7c2fcf3465487c2fdad3b35d9ab061c5f94cda2f
Suphakrit Nakhabat <suphakrit@odds.team>
2018-10-30 01:40:38 +0700

    Add httputil error

---
+++ pkg/httputil/error.go
@@ -0,0 +1,16 @@
+package httputil
+
+import "github.com/labstack/echo"
+
+func NewError(ctx echo.Context, status int, err error) error {
+	er := HTTPError{
+		Code:    status,
+		Message: err.Error(),
+	}
+	return ctx.JSON(status, er)
+}
+
+type HTTPError struct {
+	Code    int    `json:"code"`
+	Message string `json:"message"`
+}
