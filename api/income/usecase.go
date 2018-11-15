33d1a1089fde4268ec69819853418439cbbfe05a
Suphakrit Nakhabat <suphakrit@odds.team>
2018-11-15 14:25:09 +0700

    Fix: export submit date hour time zone

--- api/income/usecase.go
+++ api/income/usecase.go
@@ -137,7 +137,7 @@ func (u *usecase) ExportIncome(corporateFlag string) (string, error) {
 		income, err := u.repo.GetIncomeUserByYearMonth(user.ID.Hex(), year, month)
 		if err == nil {
 			t := income.SubmitDate
-			tf := fmt.Sprintf("%02d/%02d/%d %02d:%02d:%02d", t.Day(), int(t.Month()), t.Year(), t.Hour(), t.Minute(), t.Second())
+			tf := fmt.Sprintf("%02d/%02d/%d %02d:%02d:%02d", t.Day(), int(t.Month()), t.Year(), (t.Hour() + 7), t.Minute(), t.Second())
 			// ชื่อ, ชื่อบัญชี, เลขบัญชี, จำนวนเงินที่ต้องโอน, วันที่กรอก
 			d := []string{user.FullNameEn, user.BankAccountName, user.BankAccountNumber, utils.FormatCommas(income.NetIncome), tf}
 			strWrite = append(strWrite, d)
