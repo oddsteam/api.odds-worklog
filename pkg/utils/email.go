cb645dc05094e53482d4a9700b58a5c49d9d1185
Suphakrit Nakhabat <suphakrit@odds.team>
2018-11-20 00:44:22 +0700

    Add util validate email

---
+++ pkg/utils/email.go
@@ -0,0 +1,16 @@
+package utils
+
+import (
+	"regexp"
+)
+
+var (
+	emailRegexp = regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
+)
+
+func ValidateEmail(email string) error {
+	if !emailRegexp.MatchString(email) {
+		return ErrInvalidFormat
+	}
+	return nil
+}
