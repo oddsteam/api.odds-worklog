03d94b19e00ffbe101bbbd7ec3cba26b16335a39
Pornmongkon Pongsai <pornmongkonp@gmail.com>
2018-11-23 16:39:31 +0700

    Update reminder send api use slack client

--- api/reminder/handler.go
+++ api/reminder/handler.go
@@ -1,16 +1,13 @@
 package reminder
 
 import (
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
 	"net/http"
-	"net/url"
 
 	"github.com/labstack/echo"
 	"gitlab.odds.team/worklog/api.odds-worklog/api/income"
 	"gitlab.odds.team/worklog/api.odds-worklog/api/user"
 	"gitlab.odds.team/worklog/api.odds-worklog/pkg/mongo"
+	"gitlab.odds.team/worklog/api.odds-worklog/pkg/slack"
 	"gitlab.odds.team/worklog/api.odds-worklog/pkg/utils"
 )
 
@@ -27,26 +24,31 @@ func NewHttpHandler(r *echo.Group, session *mongo.Session) {
 	})
 }
 
+// Send Reminder godoc
+// @Summary Send Reminder
+// @Description Send Notification Reminder
+// @Tags reminder
+// @Produce  json
+// @Success 200 {array} string
+// @Failure 500 {object} utils.HTTPError
+// @Router /reminder/send [get]
 func send(c echo.Context, incomeUsecase income.Usecase) error {
-
-	// Step 1 query user
-	incomeIndividualStatusList, err := incomeUsecase.GetIncomeStatusList("N")
-	if err != nil {
-		return utils.NewError(c, 500, err)
-	}
-	incomeCorpStatusList, err := incomeUsecase.GetIncomeStatusList("Y")
-	if err != nil {
-		return utils.NewError(c, 500, err)
-	}
-	incomeStatusList := append(incomeIndividualStatusList, incomeCorpStatusList...)
+	client := slack.NewClient(TOKEN)
+	// incomeIndividualStatusList, err := incomeUsecase.GetIncomeStatusList("N")
+	// if err != nil {
+	// 	return utils.NewError(c, 500, err)
+	// }
+	// incomeCorpStatusList, err := incomeUsecase.GetIncomeStatusList("Y")
+	// if err != nil {
+	// 	return utils.NewError(c, 500, err)
+	// }
+	// incomeStatusList := append(incomeIndividualStatusList, incomeCorpStatusList...)
 	emails := []string{}
-	for _, incomeStatus := range incomeStatusList {
-		if incomeStatus.Status == "N" {
-			emails = append(emails, incomeStatus.User.Email)
-		}
-	}
-
-	// Hard Code
+	// for _, incomeStatus := range incomeStatusList {
+	// 	if incomeStatus.Status == "N" {
+	// 		emails = append(emails, incomeStatus.User.Email)
+	// 	}
+	// }
 	emails = []string{
 		"tong@odds.team",
 		"work.alongkorn@gmail.com",
@@ -55,124 +57,23 @@ func send(c echo.Context, incomeUsecase income.Usecase) error {
 		"p.watchara@gmail.com",
 		"santi@odds.team",
 	}
-
-	// Step 2 get user slack
-	slackUser, _ := getAllUserSlack()
-	slackUserIDs := []SlackPostMessageResponse{}
+	sendEmails := []string{}
+	slackUsers, err := client.GetUsersList()
+	if err != nil {
+		return utils.NewError(c, 500, err)
+	}
 	for _, email := range emails {
-		for _, member := range slackUser.Members {
+		for _, member := range slackUsers.Members {
 			if member.Profile.Email == email {
-				resChannelIDSlack, _ := getChannelIDSlack(member.ID)
-				channelID := resChannelIDSlack.Channel.ID
-				a, _ := postMessageSlack(channelID, "กรุณาเข้าไปกรอกเงินเดือนด้วยครับ") // Hard Code
-				slackUserIDs = append(slackUserIDs, a)
+				im, err := client.OpenIMChannel(member.ID)
+				if err != nil {
+					return utils.NewError(c, 500, err)
+				}
+				channelID := im.Channel.ID
+				client.PostMessage(channelID, "กรุณาเข้าไปกรอกเงินเดือนด้วยครับ")
+				sendEmails = append(sendEmails, email)
 			}
 		}
 	}
-	return c.JSON(http.StatusOK, slackUserIDs)
-}
-
-type SlackUsersResponse struct {
-	Ok      bool `json:"ok"`
-	Members []struct {
-		ID       string `json:"id"`
-		TeamID   string `json:"team_id"`
-		Name     string `json:"name"`
-		Deleted  bool   `json:"deleted"`
-		Color    string `json:"color"`
-		RealName string `json:"real_name"`
-		Tz       string `json:"tz"`
-		TzLabel  string `json:"tz_label"`
-		TzOffset int    `json:"tz_offset"`
-		Profile  struct {
-			Title                 string `json:"title"`
-			Phone                 string `json:"phone"`
-			Skype                 string `json:"skype"`
-			RealName              string `json:"real_name"`
-			RealNameNormalized    string `json:"real_name_normalized"`
-			DisplayName           string `json:"display_name"`
-			DisplayNameNormalized string `json:"display_name_normalized"`
-			StatusText            string `json:"status_text"`
-			StatusEmoji           string `json:"status_emoji"`
-			StatusExpiration      int    `json:"status_expiration"`
-			AvatarHash            string `json:"avatar_hash"`
-			Email                 string `json:"email"`
-			Image24               string `json:"image_24"`
-			Image32               string `json:"image_32"`
-			Image48               string `json:"image_48"`
-			Image72               string `json:"image_72"`
-			Image192              string `json:"image_192"`
-			Image512              string `json:"image_512"`
-			StatusTextCanonical   string `json:"status_text_canonical"`
-			Team                  string `json:"team"`
-		} `json:"profile"`
-		IsAdmin           bool `json:"is_admin"`
-		IsOwner           bool `json:"is_owner"`
-		IsPrimaryOwner    bool `json:"is_primary_owner"`
-		IsRestricted      bool `json:"is_restricted"`
-		IsUltraRestricted bool `json:"is_ultra_restricted"`
-		IsBot             bool `json:"is_bot"`
-		IsAppUser         bool `json:"is_app_user"`
-		Updated           int  `json:"updated"`
-		Has2Fa            bool `json:"has_2fa,omitempty"`
-	} `json:"members"`
-	CacheTs int `json:"cache_ts"`
-}
-
-func getAllUserSlack() (SlackUsersResponse, error) {
-	url := "https://slack.com/api/users.list?token=" + TOKEN
-	req, _ := http.NewRequest("GET", url, nil)
-	res, _ := http.DefaultClient.Do(req)
-	defer res.Body.Close()
-	body, _ := ioutil.ReadAll(res.Body)
-	var data SlackUsersResponse
-	_ = json.Unmarshal(body, &data)
-	return data, nil
-}
-
-type SlackChannelIDResponse struct {
-	Ok          bool `json:"ok"`
-	NoOp        bool `json:"no_op"`
-	AlreadyOpen bool `json:"already_open"`
-	Channel     struct {
-		ID string `json:"id"`
-	} `json:"channel"`
-}
-
-func getChannelIDSlack(userID string) (SlackChannelIDResponse, error) {
-	url := "https://slack.com/api/im.open?token=" + TOKEN + "&user=" + userID
-	req, _ := http.NewRequest("GET", url, nil)
-	res, _ := http.DefaultClient.Do(req)
-	defer res.Body.Close()
-	body, _ := ioutil.ReadAll(res.Body)
-	var data SlackChannelIDResponse
-	_ = json.Unmarshal(body, &data)
-	return data, nil
-}
-
-type SlackPostMessageResponse struct {
-	Ok      bool   `json:"ok"`
-	Channel string `json:"channel"`
-	Ts      string `json:"ts"`
-	Message struct {
-		Text     string `json:"text"`
-		Username string `json:"username"`
-		BotID    string `json:"bot_id"`
-		Type     string `json:"type"`
-		Subtype  string `json:"subtype"`
-		Ts       string `json:"ts"`
-	} `json:"message"`
-}
-
-func postMessageSlack(channelID string, text string) (SlackPostMessageResponse, error) {
-	textEncode := &url.URL{Path: text}
-	url := "https://slack.com/api/chat.postMessage?token=" + TOKEN + "&channel=" + channelID + "&text=" + textEncode.String()
-	fmt.Println(url)
-	req, _ := http.NewRequest("GET", url, nil)
-	res, _ := http.DefaultClient.Do(req)
-	defer res.Body.Close()
-	body, _ := ioutil.ReadAll(res.Body)
-	var data SlackPostMessageResponse
-	_ = json.Unmarshal(body, &data)
-	return data, nil
+	return c.JSON(http.StatusOK, sendEmails)
 }
---
+++ pkg/slack/slack.go
@@ -0,0 +1,164 @@
+package slack
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+)
+
+const baseURL string = "https://slack.com/api"
+
+type Client struct {
+	Token string
+}
+
+func NewClient(token string) *Client {
+	return &Client{
+		Token: token,
+	}
+}
+
+func (s *Client) PostMessage(channelID string, text string) (*PostMessageResponse, error) {
+	textEncode := &url.URL{Path: text}
+	url := baseURL + "/chat.postMessage?token=" + s.Token + "&channel=" + channelID + "&text=" + textEncode.String()
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, err
+	}
+	bytes, err := s.doRequest(req)
+	if err != nil {
+		return nil, err
+	}
+	var data PostMessageResponse
+	err = json.Unmarshal(bytes, &data)
+	if err != nil {
+		return nil, err
+	}
+	return &data, nil
+}
+
+func (s *Client) OpenIMChannel(userID string) (*OpenIMChannelResponse, error) {
+	url := baseURL + "/im.open?token=" + s.Token + "&user=" + userID
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, err
+	}
+	bytes, err := s.doRequest(req)
+	if err != nil {
+		return nil, err
+	}
+	var data OpenIMChannelResponse
+	err = json.Unmarshal(bytes, &data)
+	if err != nil {
+		return nil, err
+	}
+	return &data, nil
+}
+
+func (s *Client) GetUsersList() (*GetUsersList, error) {
+	url := baseURL + "/users.list?token=" + s.Token
+	fmt.Printf(url)
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, err
+	}
+	bytes, err := s.doRequest(req)
+	if err != nil {
+		return nil, err
+	}
+	var data GetUsersList
+	err = json.Unmarshal(bytes, &data)
+	if err != nil {
+		return nil, err
+	}
+	return &data, nil
+}
+
+func (s *Client) doRequest(req *http.Request) ([]byte, error) {
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	if 200 != resp.StatusCode {
+		return nil, fmt.Errorf("%s", body)
+	}
+	return body, nil
+}
+
+type GetUsersList struct {
+	Ok      bool `json:"ok"`
+	Members []struct {
+		ID       string `json:"id"`
+		TeamID   string `json:"team_id"`
+		Name     string `json:"name"`
+		Deleted  bool   `json:"deleted"`
+		Color    string `json:"color"`
+		RealName string `json:"real_name"`
+		Tz       string `json:"tz"`
+		TzLabel  string `json:"tz_label"`
+		TzOffset int    `json:"tz_offset"`
+		Profile  struct {
+			Title                 string `json:"title"`
+			Phone                 string `json:"phone"`
+			Skype                 string `json:"skype"`
+			RealName              string `json:"real_name"`
+			RealNameNormalized    string `json:"real_name_normalized"`
+			DisplayName           string `json:"display_name"`
+			DisplayNameNormalized string `json:"display_name_normalized"`
+			StatusText            string `json:"status_text"`
+			StatusEmoji           string `json:"status_emoji"`
+			StatusExpiration      int    `json:"status_expiration"`
+			AvatarHash            string `json:"avatar_hash"`
+			Email                 string `json:"email"`
+			Image24               string `json:"image_24"`
+			Image32               string `json:"image_32"`
+			Image48               string `json:"image_48"`
+			Image72               string `json:"image_72"`
+			Image192              string `json:"image_192"`
+			Image512              string `json:"image_512"`
+			StatusTextCanonical   string `json:"status_text_canonical"`
+			Team                  string `json:"team"`
+		} `json:"profile"`
+		IsAdmin           bool `json:"is_admin"`
+		IsOwner           bool `json:"is_owner"`
+		IsPrimaryOwner    bool `json:"is_primary_owner"`
+		IsRestricted      bool `json:"is_restricted"`
+		IsUltraRestricted bool `json:"is_ultra_restricted"`
+		IsBot             bool `json:"is_bot"`
+		IsAppUser         bool `json:"is_app_user"`
+		Updated           int  `json:"updated"`
+		Has2Fa            bool `json:"has_2fa,omitempty"`
+	} `json:"members"`
+	CacheTs int `json:"cache_ts"`
+}
+
+type OpenIMChannelResponse struct {
+	Ok          bool `json:"ok"`
+	NoOp        bool `json:"no_op"`
+	AlreadyOpen bool `json:"already_open"`
+	Channel     struct {
+		ID string `json:"id"`
+	} `json:"channel"`
+}
+
+type PostMessageResponse struct {
+	Ok      bool   `json:"ok"`
+	Channel string `json:"channel"`
+	Ts      string `json:"ts"`
+	Message struct {
+		Text     string `json:"text"`
+		Username string `json:"username"`
+		BotID    string `json:"bot_id"`
+		Type     string `json:"type"`
+		Subtype  string `json:"subtype"`
+		Ts       string `json:"ts"`
+	} `json:"message"`
+}
