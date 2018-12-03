121e8bc02135a0bfb2e6ded70fdf588e895500e7
Suphakrit Nakhabat <suphakrit@odds.team>
2018-12-03 23:15:24 +0700

    Add unit util error

---
+++ pkg/utils/error_test.go
@@ -0,0 +1,34 @@
+package utils
+
+import (
+	"errors"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/labstack/echo"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestConfigError(t *testing.T) {
+	assert.Equal(t, "Item not found", ErrNotFound.Error())
+	assert.Equal(t, "Cannot be Deleted", ErrCannotBeDeleted.Error())
+	assert.Equal(t, "Item already exist", ErrConflict.Error())
+	assert.Equal(t, "Invalid path", ErrInvalidPath.Error())
+	assert.Equal(t, "Invalid format", ErrInvalidFormat.Error())
+	assert.Equal(t, "Invalid token", ErrInvalidToken.Error())
+	assert.Equal(t, "Bad request", ErrBadRequest.Error())
+}
+
+func TestNewError(t *testing.T) {
+	e := echo.New()
+	req := httptest.NewRequest(echo.GET, "/", strings.NewReader(""))
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+
+	NewError(c, http.StatusOK, errors.New("ok"))
+
+	assert.Equal(t, http.StatusOK, rec.Code)
+	assert.Equal(t, `{"code":200,"message":"ok"}`, rec.Body.String())
+}
