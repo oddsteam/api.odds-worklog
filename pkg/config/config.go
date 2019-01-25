e3505765af75b2ec5a6cf1bd4ac00cc9748f58dd
Suphakrit Nakhabat <suphakrit@odds.team>
2019-01-25 23:21:26 +0700

    Remove check load .env file

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,19 +1,13 @@
 package config
 
 import (
-	"log"
 	"os"
 	"strconv"
 
-	"github.com/joho/godotenv"
 	"gitlab.odds.team/worklog/api.odds-worklog/models"
 )
 
 func Config() *models.Config {
-	if err := godotenv.Load(); err != nil {
-		log.Fatal("Error loading .env file")
-	}
-
 	cp, _ := strconv.Atoi(os.Getenv("MONGO_DB_CONECTION_POOL"))
 	config := models.Config{
 		os.Getenv("MONGO_DB_HOST"),
