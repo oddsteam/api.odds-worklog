ca84e484ab33911f81f6647797b04d2d8e8c5be5
juacompe <juacompe@gmail.com>
2022-12-07 19:40:52 +0700

    get_student_loan script no longer hardcoded

--- scripts/get_student_loan.go
+++ scripts/get_student_loan.go
@@ -8,12 +8,14 @@ import (
 	"net/http"
 	"os"
 	"strings"
+	"time"
 
 	"github.com/globalsign/mgo/bson"
 	"gitlab.odds.team/worklog/api.odds-worklog/api/income"
 	"gitlab.odds.team/worklog/api.odds-worklog/models"
 	"gitlab.odds.team/worklog/api.odds-worklog/pkg/config"
 	"gitlab.odds.team/worklog/api.odds-worklog/pkg/mongo"
+	"gitlab.odds.team/worklog/api.odds-worklog/pkg/utils"
 )
 
 func main() {
@@ -43,7 +45,8 @@ func getStudentLoans(sessionId string, csrf string) ([]byte, error) {
 	url := "https://slfrd.dsl.studentloan.or.th/SLFRD/EmployeeReport/getDataByPage"
 	method := "POST"
 
-	payload := strings.NewReader(`deleteFlag=&month=12&year=2565`)
+	y, m := utils.GetYearMonthInBuddistEra(time.Now())
+	payload := strings.NewReader(fmt.Sprintf(`deleteFlag=&month=%d&year=%d`, m, y))
 
 	req, err := http.NewRequest(method, url, payload)
 
