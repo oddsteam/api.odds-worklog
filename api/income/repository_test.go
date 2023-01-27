eb37b23d354fd902c545d9bd0f2113748bcc09e3
juacompe <juacompe@gmail.com>
2023-01-27 09:42:10 +0700

    remove old test

--- api/income/repository_test.go
+++ api/income/repository_test.go
@@ -2,17 +2,10 @@ package income
 
 import (
 	"testing"
-	"time"
 
 	"github.com/globalsign/mgo"
 	"github.com/stretchr/testify/assert"
 )
-
-func TestGetStudentLoansQueryWithCurrentMonth(t *testing.T) {
-	n := time.Date(2022, time.Month(11), 1, 13, 30, 29, 0, time.UTC)
-	query := loanQuery(n)
-	assert.Equal(t, "11/2565", query["list.monthYear"])
-}
 
 func TestAdminCanExportIndividualIncomeWithoutStudentLoans(t *testing.T) {
 	defer func() {
