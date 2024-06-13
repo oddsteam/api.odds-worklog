fd1641818db0b832442abebbfc17a9e9a2cea900
juacompe <juacompe@gmail.com>
2024-06-13 19:43:51 +0700

    move logic into model class

--- api/income/model.go
+++ api/income/model.go
@@ -1,6 +1,8 @@
 package income
 
 import (
+	"time"
+
 	"gitlab.odds.team/worklog/api.odds-worklog/models"
 )
 
@@ -66,4 +68,50 @@ func (i *Income) prepareDataForAddIncome(req models.IncomeReq, userDetail models
 	}
 
 	return &income, nil
+}
+
+func (i *Income) prepareDataForUpdateIncome(req models.IncomeReq, userDetail models.User, income *models.Income) error {
+	ins, err := calIncomeSum(req.WorkDate, userDetail.Vat, userDetail.DailyIncome, userDetail.GetRole())
+	if err != nil {
+		return err
+	}
+	insSpecial, err := calIncomeSum(req.WorkingHours, userDetail.Vat, req.SpecialIncome, userDetail.GetRole())
+	if err != nil {
+		return err
+	}
+	summaryIncome, err := calSummary(ins.TotalIncome, insSpecial.TotalIncome)
+	if err != nil {
+		return err
+	}
+	summaryNetIncome, err := calSummary(ins.Net, insSpecial.Net)
+	if err != nil {
+		return err
+	}
+	summaryWht, err := calSummary(ins.WHT, insSpecial.WHT)
+	if err != nil {
+		return err
+	}
+	var summaryVat string
+	if userDetail.Vat != "N" {
+		summaryVat, err = calSummary(ins.VAT, insSpecial.VAT)
+		if err != nil {
+			return err
+		}
+	} else {
+		summaryVat = ""
+	}
+
+	income.SubmitDate = time.Now()
+	income.TotalIncome = summaryIncome
+	income.NetIncome = summaryNetIncome
+	income.NetSpecialIncome = insSpecial.Net
+	income.NetDailyIncome = ins.Net
+	income.VAT = summaryVat
+	income.WHT = summaryWht
+	income.Note = req.Note
+	income.WorkDate = req.WorkDate
+	income.SpecialIncome = req.SpecialIncome
+	income.WorkingHours = req.WorkingHours
+
+	return nil
 }
--- api/income/update_income.go
+++ api/income/update_income.go
@@ -1,8 +1,6 @@
 package income
 
 import (
-	"time"
-
 	"gitlab.odds.team/worklog/api.odds-worklog/models"
 )
 
@@ -13,47 +11,11 @@ func (u *usecase) UpdateIncome(id string, req *models.IncomeReq, uid string) (*m
 		return nil, err
 	}
 
-	ins, err := calIncomeSum(req.WorkDate, userDetail.Vat, userDetail.DailyIncome, userDetail.GetRole())
+	err = NewIncome(uid).prepareDataForUpdateIncome(*req, *userDetail, income)
 	if err != nil {
 		return nil, err
 	}
-	insSpecial, err := calIncomeSum(req.WorkingHours, userDetail.Vat, req.SpecialIncome, userDetail.GetRole())
-	if err != nil {
-		return nil, err
-	}
-	summaryIncome, err := calSummary(ins.TotalIncome, insSpecial.TotalIncome)
-	if err != nil {
-		return nil, err
-	}
-	summaryNetIncome, err := calSummary(ins.Net, insSpecial.Net)
-	if err != nil {
-		return nil, err
-	}
-	summaryWht, err := calSummary(ins.WHT, insSpecial.WHT)
-	if err != nil {
-		return nil, err
-	}
-	var summaryVat string
-	if userDetail.Vat != "N" {
-		summaryVat, err = calSummary(ins.VAT, insSpecial.VAT)
-		if err != nil {
-			return nil, err
-		}
-	} else {
-		summaryVat = ""
-	}
-
-	income.SubmitDate = time.Now()
-	income.TotalIncome = summaryIncome
-	income.NetIncome = summaryNetIncome
-	income.NetSpecialIncome = insSpecial.Net
-	income.NetDailyIncome = ins.Net
-	income.VAT = summaryVat
-	income.WHT = summaryWht
-	income.Note = req.Note
-	income.WorkDate = req.WorkDate
-	income.SpecialIncome = req.SpecialIncome
-	income.WorkingHours = req.WorkingHours
 	u.repo.UpdateIncome(income)
+
 	return income, nil
 }
