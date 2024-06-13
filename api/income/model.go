377d50d21d1d95f6aba2b2961b98cfeeb0150d91
juacompe <juacompe@gmail.com>
2024-06-13 19:27:51 +0700

    refactor: remove unnecessary fields

--- api/income/model.go
+++ api/income/model.go
@@ -5,14 +5,7 @@ import (
 )
 
 type Income struct {
-	UserID              string
-	NetIncomeStr        string
-	NetDailyIncomeStr   string
-	NetSpecialIncomeStr string
-	VATStr              string
-	WHTStr              string
-	TotalIncomeStr      string
-	ThaiCitizenID       string
+	UserID string
 }
 
 func NewIncome(uidFromSession string) *Income {
@@ -51,22 +44,16 @@ func (i *Income) prepareDataForAddIncome(req models.IncomeReq, userDetail models
 	if err != nil {
 		return nil, err
 	}
-	i.NetIncomeStr = summaryNetIncome
-	i.NetDailyIncomeStr = ins.Net
-	i.NetSpecialIncomeStr = insSpecial.Net
-	i.VATStr = summaryVat
-	i.WHTStr = summaryWht
-	i.TotalIncomeStr = summaryIncome
 
 	income := models.Income{
 		UserID:            i.UserID,
-		TotalIncome:       i.TotalIncomeStr,
-		NetIncome:         i.NetIncomeStr,
-		NetSpecialIncome:  i.NetSpecialIncomeStr,
-		NetDailyIncome:    i.NetDailyIncomeStr,
+		TotalIncome:       summaryIncome,
+		NetIncome:         summaryNetIncome,
+		NetSpecialIncome:  insSpecial.Net,
+		NetDailyIncome:    ins.Net,
 		Note:              req.Note,
-		VAT:               i.VATStr,
-		WHT:               i.WHTStr,
+		VAT:               summaryVat,
+		WHT:               summaryWht,
 		WorkDate:          req.WorkDate,
 		SpecialIncome:     req.SpecialIncome,
 		WorkingHours:      req.WorkingHours,
