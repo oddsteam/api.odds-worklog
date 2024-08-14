f0e8f2a72c4be24131312dd7773be4ca7171ad0f
juacompe <juacompe@gmail.com>
2024-08-14 14:05:47 +0700

    mock queue data

---
+++ api/queue/queue.go
@@ -0,0 +1,51 @@
+package queue
+
+import (
+	"log"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Panicf("%s: %s", msg, err)
+	}
+}
+
+func Connect() *amqp.Connection {
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	failOnError(err, "Failed to connect to RabbitMQ")
+	return conn
+}
+
+func GetChannel(conn *amqp.Connection) *amqp.Channel {
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	return ch
+}
+
+func DeclareQueue(ch *amqp.Channel, name string, durable bool) amqp.Queue {
+	q, err := ch.QueueDeclare(
+		name,
+		durable,
+		false, // delete when unused
+		false, // exclusive
+		false, // no-wait
+		nil,   // arguments
+	)
+	failOnError(err, "Failed to declare a queue")
+	return q
+}
+
+func Publish(ch *amqp.Channel, routingKey string, body string) {
+	err := ch.Publish(
+		"",         // exchange
+		routingKey, // routing key (queue name)
+		false,      // mandatory
+		false,      // immediate
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte(body),
+		})
+	failOnError(err, "Failed to publish a message")
+}
