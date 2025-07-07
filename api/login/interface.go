590baa99e23cfd144aa75acf37b620c039d9193d
juacompe <juacompe@gmail.com>
2025-07-07 17:40:54 +0700

    remove google login

--- api/login/interface.go
+++ api/login/interface.go
@@ -2,11 +2,9 @@ package login
 
 import (
 	"gitlab.odds.team/worklog/api.odds-worklog/models"
-	oauth2 "google.golang.org/api/oauth2/v2"
 )
 
 type Usecase interface {
-	GetTokenInfo(idToken string) (*oauth2.Tokeninfo, error)
 	ValidateAndExtractToken(idToken string) (models.Identity, error)
 	CreateUser(email string) (*models.User, error)
 	CreateUserAndValidateEmail(email string) (*models.User, error)
