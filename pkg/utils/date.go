876e80a9ff74e6360731ba1bab4ae479b35f40e1
Chokchai Phatharamalai <juacompe@gmail.com>
2026-02-17 13:25:24 +0700

    remove duplications

--- pkg/utils/date.go
+++ pkg/utils/date.go
@@ -5,29 +5,6 @@ import (
 	"time"
 )
 
-func GetNow() string {
-	return getNow(time.Now())
-}
-
-func getNow(now time.Time) string {
-	return now.Format(time.RFC3339)
-}
-
-func ParseDate(s string) (time.Time, error) {
-	layout := time.RFC3339Nano
-	return time.Parse(layout, s)
-}
-
-func GetCurrentMonth() string {
-	return getCurrentMonth(time.Now())
-}
-
-func getCurrentMonth(now time.Time) string {
-	y, m, _ := now.Date()
-	cm := fmt.Sprintf("%d-%d", y, int(m))
-	return cm
-}
-
 func GetYearMonthStringInBuddistEra(now time.Time) (string, string) {
 	y, m, _ := now.Date()
 	return fmt.Sprintf("%d", y+543), fmt.Sprintf("%02d", m)
--- pkg/utils/date_test.go
+++ pkg/utils/date_test.go
@@ -1,47 +1,11 @@
 package utils
 
 import (
-	"regexp"
 	"testing"
 	"time"
 
 	"github.com/stretchr/testify/assert"
 )
-
-func TestGetNow(t *testing.T) {
-	n := GetNow()
-	r := regexp.MustCompile("\\d{4}-\\d{0,2}-\\d{0,2}T\\d{2}:\\d{2}:\\d{2}")
-	assert.True(t, r.MatchString(n))
-}
-
-func TestGetNowAgain(t *testing.T) {
-	n := time.Date(2022, time.Month(12), 1, 13, 30, 29, 0, time.UTC)
-	actual := getNow(n)
-	assert.Equal(t, "2022-12-01T13:30:29Z", actual)
-}
-
-func TestParseDateSuccess(t *testing.T) {
-	actual, _ := ParseDate("2022-12-01T13:30:29Z")
-	expected := time.Date(2022, time.Month(12), 1, 13, 30, 29, 0, time.UTC)
-	assert.Equal(t, expected, actual)
-}
-func TestParseAnotherDate(t *testing.T) {
-	actual, _ := ParseDate("2024-07-26T06:26:25.531Z")
-	expected := time.Date(2024, time.Month(7), 26, 6, 26, 25, 531000000, time.UTC)
-	assert.Equal(t, expected, actual)
-}
-
-func TestGetCurrentMonth(t *testing.T) {
-	r := regexp.MustCompile("\\d{4}-\\d{0,2}")
-	cm := GetCurrentMonth()
-	assert.True(t, r.MatchString(cm))
-}
-
-func TestGetCurrentMonthAgain(t *testing.T) {
-	n := time.Date(2022, time.Month(12), 1, 13, 30, 29, 0, time.UTC)
-	actual := getCurrentMonth(n)
-	assert.Equal(t, "2022-12", actual)
-}
 
 func TestGetYearMonthStringInBuddistEra(t *testing.T) {
 	n := time.Date(2022, time.Month(1), 1, 13, 30, 29, 0, time.UTC)
